9765abd63fa0d971eeb45a391c5480a326c60fa5
uuuand888 <uuu_888@qq.com>
2017-04-23 21:29:34 +0800

    add

--- Client/Client.java
+++ Client/Client.java
@@ -2,3 +2,24 @@
  * Client
  *
  */
+import java.io.*;
+import java.net.*;
+public class Client{
+    public static void main(String[] args){
+        String s = null;
+        Socket mySocket;
+        DataInputStream in = null;
+        DataOutputStream out = null;
+        try{
+            mySocket = new Socket("127.0.0.1",4441);
+            in = new DataInputStream(mySocket.getInputStream());
+            out = new DataOutputStream(mySocket.getOutputStream());
+            out.writeUTF("hello server!");
+            s = in.readUTF();
+            System.out.println(s);
+            mySocket.close();
+        }catch(IOException e){
+            System.out.println("can’t connect");
+        }
+    }
+}
--- Server/Server.java
+++ Server/Server.java
@@ -2,3 +2,56 @@
  *server
  *
  */
+import java.io.*;
+import java.net.*;
+class React implements Runnable {
+	   	Thread t;
+	   	Socket fd;
+        DataOutputStream out = null;
+        DataInputStream in = null;
+	   	React(String TName, Socket fd) {
+	        // Create a new, second thread
+	        t = new Thread(this, TName);
+	        this.fd=fd;
+	        System.out.println("Child thread: " + t);
+	        t.start(); // Start the thread
+	    }
+
+	    // This is the entry point for the second thread.
+	    public void run() {
+	        try {
+	            in = new DataInputStream(fd.getInputStream());
+	            out = new DataOutputStream(fd.getOutputStream());
+		    	while (true){
+		                String s = in.readUTF();
+		                if(s==null) 
+		                	break;
+		                System.out.println(s);
+		                out.writeUTF("客户，你好，我是服务器"+Thread.currentThread().getName());
+		    	}
+	    		in.close();
+	    		out.close();
+	    	}catch(IOException e) {
+	    		System.out.println("ERROR:"+e);
+	    	}
+	        System.out.println("Exiting child thread.");
+	    }	
+}
+
+public class Server{
+    public static void main(String args[]){
+        ServerSocket server = null;
+        Socket you = null;
+		int	i=0;
+        try{
+            server = new ServerSocket(4441);
+        	while(true) {
+        		you = server.accept();
+        		new React("Thread_"+i, you);
+        		i++;
+        	}
+        }catch(IOException e1){
+            System.out.println("ERROR:"+e1);
+        }
+    }
+}
